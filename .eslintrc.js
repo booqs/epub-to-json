74b4068976aff8a01e90bc325498550f7b8017f0
ashakirzianov <ashakirzianov@icloud.com>
2020-04-23 00:22:21 -0300

    Initial

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    "@typescript-eslint/member-delimiter-style": ["error", {
+      "multiline": {
+        "delimiter": "comma",
+        "requireLast": true
+      },
+      "singleline": {
+        "delimiter": "comma",
+        "requireLast": true
+      },
+    }],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "space-before-function-paren": "off",
+    "indent": "off",
+    "semi": ["error", "always"],
+    "quotes": ["error", "single"],
+    "generator-star-spacing": ["error", "after"],
+    "comma-dangle": ["off", "always"],
+    "no-unneeded-ternary": "off",
+  },
+}
